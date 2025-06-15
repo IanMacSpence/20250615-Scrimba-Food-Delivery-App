569e32248f69e5c1fd403113fb304ce4fccfa721
Ian MacDonald Spence <50440569+IanMacSpence@users.noreply.github.com>
2025-06-15 16:47:26 +0200

    Add header and menu section

--- index.js
+++ index.js
@@ -9,21 +9,22 @@ function getMenuHtml() {
   menuArray.forEach(function (item) {
     console.log(item);
   });
-  const menuHtml = menuArray.map((menuItem) => {
+  const menuHtmlArray = menuArray.map((menuItem) => {
     return ` <div class="item-card">
-                                <div class="item-pic">${menuItem.emoji}</div>
-                                <div class="details-container">
-                                    <h3 class="item-name">${menuItem.name}</h3>
-                                    <p class="ingredients">${menuItem.ingredients.join(
-                                      ", "
-                                    )}</p>
-                                    <p class="price">${menuItem.price}</p>
-                                </div>
-                                <div class="addButton">+</div>
-                            </div>`;
+              <div class="item-graphic">${menuItem.emoji}</div>
+              <div class="item-details-container">
+                  <h3 class="item-title">${menuItem.name}</h3>
+                  <p class="item-description">${menuItem.ingredients.join(
+                    ", "
+                  )}</p>
+                  <p class="item-price">${menuItem.price}</p>
+              </div>
+              <div class="add-btn">+</div>
+            </div>`;
   });
 
-  menuHtml.forEach((item) => console.log(item));
+  menuHtmlArray.forEach((item) => console.log(item));
+  return menuHtmlArray;
 }
 
 /* Attach the array to the parent */
